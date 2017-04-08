60f090905023b0d3aadc60ce49900a14a306be31
Ethan Wells <ethanialw@gmail.com>
2017-04-08 23:07:08 +0800

    Created git whatchanged to HTML script for ease of access offline & engineering notebook purposes

---
+++ 2017/Planning/tools/cleanuphelp.py
@@ -0,0 +1,90 @@
+import sys, chardet
+
+def indexInStr(index, string):
+    out = 0
+    for i in range(len(string)):
+        if string[i] == index:
+            break
+        out += 1
+    return out
+
+def styleContent(date, commitkey, author, description, filesModified, filesAdded, filesDeleted):
+    out = "<div class='commit'>"
+    out += "<p><b>DATE:</b> " + date + "</p>\n"
+    out += "<p><b>AUTHOR:</b> " + author + "</p>\n"
+    out += "<p><b>KEY:</b> " + commitkey + "</p>\n"
+    out += "<em style='color: #0000EE;' onclick='window.open(\"https://github.com/iuyte/VEX-709s/tree/" + commitkey + "\")'>" + "Browse at this point in repo" + "</em>\n"
+    out += "<p><b>DESCRIPTION:</b><br>" + description + "</p>\n"
+    if len(filesAdded) > 0:
+        out += "<b>Files added:</b>\n<ul>\n"
+        for i in range(len(filesAdded)):
+            out += "<li>" + filesAdded[i] + "</li>\n"
+        out += "</ul>"
+    if len(filesModified) > 0:
+        out += "<b>Files modified:</b>\n<ul>\n"
+        for i in range(len(filesModified)):
+            out += "<li>" + filesModified[i] + "</li>\n"
+        out += "</ul>"
+    if len(filesDeleted) > 0:
+        out += "<b>Files deleted:</b>\n<ul>\n"
+        for i in range(len(filesDeleted)):
+            out += "<li>" + filesDeleted[i] + "</li>\n"
+        out += "</ul>"
+    out += "<br></div>\n"
+    return out
+
+def removeSpaces(content):
+    content = content.split(" ")
+    out = ""
+    for i in range(len(content)):
+        out += content[i]
+    return out
+
+def parseContent(content):
+    if content == "":
+        assert False, "File is empty or does not exist"
+    out = "<html>\n<head>\n<title>Git Commit history</title>\n</head>\n<body>"
+    count = 0
+    commits = content.split("commit ")
+    out += "<h1>" + str(len(commits)) + " Commits</h1>\n<br>\n"
+    commit = 0
+    for commit in range(1, len(commits)):
+        aline = 0
+        lines = commits[commit].split("\n")
+        try:
+            lines[aline]
+        except:
+            continue
+        line = ""
+        lline = ""
+        date = ""
+        commitkey = ""
+        author = ""
+        description = ""
+        filesModified = []
+        filesAdded = []
+        filesDeleted = []
+        try:
+            commitkey = lines[aline]
+            aline += 1
+            author = lines[aline].split("Author:")[1]
+            aline += 1
+            date = lines[aline].split("Date: ")[1]
+            aline += 2
+            description = lines[aline][4:]
+            aline += 2
+        except:
+            continue
+        filesModified = []
+        filesAdded = []
+        filesDeleted = []
+        for aline in range(aline, len(lines)):
+            if ". M" in lines[aline]:
+                filesModified.append(lines[aline][39:])
+            elif ". A" in lines[aline]:
+                filesAdded.append(lines[aline][39:])
+            elif ". D" in lines[aline]:
+                filesDeleted.append(lines[aline][39:])
+        out += styleContent(date, commitkey, author, description, filesModified, filesAdded, filesDeleted)
+    out += "</body>\n</html>\n"
+    return out
